867d1f37ab5a0f7bcb3addeff564522cf2e1f9f1
Ed Chaparro <ed@IntrepidProducts.com>
2022-07-25 14:19:53 -0400

    Add Person class

---
+++ src/App/OrgChart/EntityAbstract.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IntrepidProducts.OrgChart
+{
+    public interface IHasId
+    {
+        Guid Id { get; }
+    }
+
+    public abstract class EntityAbstract : IHasId
+    {
+        protected EntityAbstract(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; }
+    }
+}
---
+++ src/App/OrgChart/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IntrepidProducts.OrgChart
+{
+    public class Person : EntityAbstract
+    {
+        public Person(Guid id) : base(id)
+        {}
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
