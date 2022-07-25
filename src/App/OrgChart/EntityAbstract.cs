bc101f4a971e4ef048e2d13652f0bd52ea85713d
Ed Chaparro <ed@IntrepidProducts.com>
2022-07-25 14:55:11 -0400

    Added Person methods reflecting org structure

--- src/App/OrgChart/EntityAbstract.cs
+++ src/App/OrgChart/EntityAbstract.cs
@@ -11,9 +11,31 @@ namespace IntrepidProducts.OrgChart
     {
         protected EntityAbstract(Guid id)
         {
+            if (id == Guid.Empty)
+            {
+                throw new ArgumentException("Cannot initiate Entity with an Empty Guid");
+            }
+
             Id = id;
         }
 
         public Guid Id { get; }
+
+        #region Equality
+        public override bool Equals(object obj)
+        {
+            return Id.Equals(obj);
+        }
+
+        protected bool Equals(EntityAbstract other)
+        {
+            return Id.Equals(other.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+        #endregion
     }
 }
--- src/App/OrgChart/Person.cs
+++ src/App/OrgChart/Person.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace IntrepidProducts.OrgChart
 {
@@ -11,5 +13,38 @@ namespace IntrepidProducts.OrgChart
         public string LastName { get; set; }
 
         public string FullName => $"{FirstName} {LastName}";
+
+        public string Title { get; set; }
+
+        public Person? ReportsTo { get; set; }
+        public bool IsManaged => ReportsTo != null;
+
+        #region Direct Reports
+        private readonly List<Person> _directReports = new List<Person>();
+        public IEnumerable<Person> DirectReports => _directReports;
+        public bool IsManager => DirectReports.Any();
+
+        public bool AddDirectReport(Person person)
+        {
+            //TODO: Check if Person is valid
+            if (_directReports.Contains(person))
+            {
+                return false;
+            }
+
+            _directReports.Add(person);
+            return true;
+        }
+
+        public bool RemoveDirectReport(Person person)
+        {
+            if (!_directReports.Contains(person))
+            {
+                return false;
+            }
+
+            return _directReports.Remove(person);
+        }
+        #endregion
     }
 }
